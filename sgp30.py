e8c19b28e05a329a56d93d53de2f7ac8eca048da
Robert Hughes <robert.hughes4@ocado.com>
2019-06-07 18:06:10 +0100

    shorten unnecessarily long filename

--- sgp30.py
+++ sgp30.py
@@ -1,61 +1,16 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2017 ladyada for Adafruit Industries
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-"""
-`adafruit_sgp30`
-====================================================
-
-I2C driver for SGP30 Sensirion VoC sensor
-
-* Author(s): ladyada, Alexandre Marquet.
-
-Implementation Notes
---------------------
-
-**Hardware:**
-
-* Adafruit `SGP30 Air Quality Sensor Breakout - VOC and eCO2
-  <https://www.adafruit.com/product/3709>`_ (Product ID: 3709)
-
-**Software and Dependencies:**
-
-* MicroPython:
-    https://github.com/micropython/micropython
-
-"""
 import time
 from micropython import const
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/alexmrqt/Adafruit_CircuitPython_SGP30.git"
 
+_SGP30_DEFAULT_I2C_ADDR = const(0x58)
+_SGP30_FEATURESET = const(0x0020)
 
-# pylint: disable=bad-whitespace
-_SGP30_DEFAULT_I2C_ADDR  = const(0x58)
-_SGP30_FEATURESET        = const(0x0020)
+_SGP30_CRC8_POLYNOMIAL = const(0x31)
+_SGP30_CRC8_INIT = const(0xFF)
+_SGP30_WORD_LEN = const(2)
 
-_SGP30_CRC8_POLYNOMIAL   = const(0x31)
-_SGP30_CRC8_INIT         = const(0xFF)
-_SGP30_WORD_LEN          = const(2)
-# pylint: enable=bad-whitespace
 
 class Adafruit_SGP30:
     """
@@ -78,30 +33,25 @@ class Adafruit_SGP30:
             raise RuntimeError('SGP30 Not detected')
         self.iaq_init()
 
-
     @property
     def tvoc(self):
         """Total Volatile Organic Compound in parts per billion."""
         return self.iaq_measure()[1]
-
 
     @property
     def baseline_tvoc(self):
         """Total Volatile Organic Compound baseline value"""
         return self.get_iaq_baseline()[1]
 
-
     @property
-    def co2eq(self):
+    def co2_equivalent(self):
         """Carbon Dioxide Equivalent in parts per million"""
         return self.iaq_measure()[0]
-
 
     @property
     def baseline_co2eq(self):
         """Carbon Dioxide Equivalent baseline value"""
         return self.get_iaq_baseline()[0]
-
 
     def iaq_init(self):
         """Initialize the IAQ algorithm"""
@@ -113,13 +63,14 @@ class Adafruit_SGP30:
         # name, command, signals, delay
         return self._run_profile(["iaq_measure", [0x20, 0x08], 2, 0.05])
 
-    def get_iaq_baseline(self):
-        """Retreive the IAQ algorithm baseline for CO2eq and TVOC"""
+    @property
+    def iaq_baseline(self):
+        """Get the IAQ algorithm baseline for CO2eq and TVOC"""
         # name, command, signals, delay
         return self._run_profile(["iaq_get_baseline", [0x20, 0x15], 2, 0.01])
 
-
-    def set_iaq_baseline(self, co2eq, tvoc):
+    @iaq_baseline.setter
+    def iaq_baseline(self, co2eq, tvoc):
         """Set the previously recorded IAQ algorithm baseline for CO2eq and TVOC"""
         if co2eq == 0 and tvoc == 0:
             raise RuntimeError('Invalid baseline')
